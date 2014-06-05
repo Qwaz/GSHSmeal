cec7be0dcc6ceb75fc9df9f81de331e0fe628363
Qwaz <qwazpia@gmail.com>
2014-06-05 12:33:50 +0900

    Login Page Redesigned

--- Main/forms.py
+++ Main/forms.py
@@ -14,8 +14,8 @@ class LoginForm(BootstrapForm):
 	id = forms.CharField()
 	password = forms.CharField(widget=forms.PasswordInput, required=False)
 
-	jsession_id = forms.CharField()
-	m = forms.CharField(required=False)
+	jsession_id = forms.CharField(widget=forms.HiddenInput)
+	m = forms.CharField(widget=forms.HiddenInput, required=False)
 
 	secure_id = forms.CharField(widget=forms.HiddenInput)
 	secure_password = forms.CharField(widget=forms.HiddenInput)
