984432d71ecfc158adf6b35cb91fd45324679ceb
Qwaz <qwazpia@gmail.com>
2014-08-20 18:35:36 +0900

    어드민 사이트 Food 모델 정렬 및 검색 활성화

--- meals/admin.py
+++ meals/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from meals.models import Meal, Food, Menu
 
 
+class FoodAdmin(admin.ModelAdmin):
+	ordering = ['name']
+	search_fields = ['name']
+
+
 admin.site.register(Meal)
-admin.site.register(Food)
+admin.site.register(Food, FoodAdmin)
 admin.site.register(Menu)
